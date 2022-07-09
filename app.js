e81a94c66856ae9bfdde1abea1b9daebc73f9dea
LeonarDev <leonardo.majevski@gmail.com>
2022-07-09 17:54:42 -0300

    BACK-04 - Create the Register Endpoint

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+const bcrypt = require("bcrypt");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const dbConnect = require("./db/dbConnect");
+const User = require("./db/userModel");
 
 // body parser configuration
 app.use(bodyParser.json());
@@ -14,5 +16,38 @@ app.get("/", (request, response, next) => {
 
 // execute database connection
 dbConnect();
+
+app.post("/register", (request, response) => {
+  // The code above is telling bcrypt to hash the password received from request body 10 times or 10 salt rounds
+  bcrypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: request.body.email,
+        password: hashedPassword,
+      });
+
+      user
+        .save()
+        .then((result) => {
+          response.status(201).send({
+            message: "User Created Successfully",
+            result,
+          });
+        })
+        .catch((error) => {
+          response.status(500).send({
+            message: "Error creating user",
+            error,
+          });
+        });
+    })
+    .catch((e) => {
+      response.status(500).send({
+        message: "Password was not hashed successfully",
+        e,
+      });
+    });
+});
 
 module.exports = app;
