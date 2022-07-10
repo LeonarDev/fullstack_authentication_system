9dd2f33e3a25419e2656cf48c6f6e2b194f424d7
LeonarDev <leonardo.majevski@gmail.com>
2022-07-09 21:26:31 -0300

    BACK-07 - Handle CORS Errors

--- app.js
+++ app.js
@@ -19,6 +19,20 @@ app.get("/", (request, response, next) => {
 // execute database connection
 dbConnect();
 
+// Curb Cores Error by adding a header here
+app.use((request, response, next) => {
+  request.setHeader("Access-Control-Allow-Origin", "*");
+  request.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  request.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 app.post("/register", (request, response) => {
   // The code above is telling bcrypt to hash the password received from request body 10 times or 10 salt rounds
   bcrypt
